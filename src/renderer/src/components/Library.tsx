bebb2a00cbd2d91f15e3cd196538e915287ae755
JohnSalinas123 <salinasjohn257@gmail.com>
2025-02-01 12:05:11 -0800

    updated library styling and design to make use of mantine components and work with main's file protocol

--- src/renderer/src/components/Library.tsx
+++ src/renderer/src/components/Library.tsx
@@ -1,80 +1,170 @@
 import { useState, useEffect } from 'react'
 import { pdfjs } from 'react-pdf'
 import { IoIosAddCircleOutline } from 'react-icons/io'
-import { LibraryItem } from './LibraryItem'
-//import pdfWorker from 'pdfjs-dist/build/pdf.worker.js?worker'
 
 import './Library.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
+import { Button, FileButton, Group, Loader, Paper, Stack, Image, Text } from '@mantine/core'
+
+import classes from './Library.module.css'
 
 pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs'
 
-interface PDFFileData {
+interface PdfBookData {
   title: string | null
-  path: string
-  pageCount: number
+  file_path: string
+  num_pages: number
+  thumbnail_path: string
 }
 
-type IpcRendererEvent = {
-  sender: Electron.IpcRenderer
-  returnValue?: unknown
-}
+//type IpcRendererEvent = {
+//  sender: Electron.IpcRenderer
+//  returnValue?: unknown
+//}
 
 export const Library: React.FC = () => {
-  const [pdfFilesData, setPdfFilesData] = useState<PDFFileData[]>([])
+  const [pdfBooksData, setPdfBooksData] = useState<PdfBookData[]>([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    window.electron.ipcRenderer.send('fetch-pdf-file')
 
+    // fetch pdf books data to load initial view with user's books
+    fetchPdfBooks()
+
+
+    //window.electron.ipcRenderer.send('fetch-pdf-books')
+
+    /*
     window.electron.ipcRenderer.on(
       'pdf-data',
-      (_event: IpcRendererEvent, pdfFileDataObjs: PDFFileData[]) => {
-        setPdfFilesData(pdfFileDataObjs)
+      (_event: IpcRendererEvent, pdfFileDataObjs: PdfBookData[]) => {
+        setPdfBooksData(pdfFileDataObjs)
         setLoading(false)
       }
     )
+    */
 
-    /*
-        const watcher = watch(directory, (eventType, filename) => {
-            if (filename && filename.endsWith('.pdf')) {
-                // File change detected, re-fetch the list of PDF files
-                const updatedFiles = readdirSync(directory)
-                    .filter(fileName => fileName.endsWith('.pdf'))
-                setPdfFiles(updatedFiles);
-            }
-        });
-        */
+    window.electron.ipcRenderer.on('pdf-added', (_event, newBookData) => {
+      setPdfBooksData((prevBookData) => [...prevBookData, newBookData])
+    })
+
+    return (): void => {
+      window.electron.ipcRenderer.removeAllListeners('pdf-data')
+    }
   }, [])
+  
+  const fetchPdfBooks = async(): Promise<void> => {
+    try {
+      const pdfBooksData = await window.electron.ipcRenderer.invoke('fetch-pdf-books')
+      setPdfBooksData(pdfBooksData)
+      setLoading(false)
+    } catch (error) {
+      console.log('Error fetching PDF books:', error)
+    }
+
+  }
+
+  const handleFileSelect = (file: File | null): void => {
+    if (!file) return
+
+    // send fild path to main process
+    window.electron.ipcRenderer.send('save-pdf', file.path)
+  }
 
   return (
     <>
       {loading ? (
-        <div className="loading-container">
-          <div>Loading...</div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            width: '100vw'
+          }}
+        >
+          <Loader color="blue" size="xl" type="dots" />
         </div>
       ) : (
-        <div className="library-container">
-          <LibrayAddItem />
-          {pdfFilesData &&
-            pdfFilesData.map((file, index) => (
-              <LibraryItem key={index} pdfPath={file.path} pdfTitle={file.title} />
-            ))}
-        </div>
+        <>
+          <Stack gap={0}>
+            <Group className={classes['control-bar']}>
+              <FileButton onChange={(file) => handleFileSelect(file)} accept="application/pdf">
+                {(props) => (
+                  <Button {...props} radius="xl" rightSection={<IoIosAddCircleOutline />}>
+                    Add new book
+                  </Button>
+                )}
+              </FileButton>
+            </Group>
+
+            <div className="library-grid">
+              {pdfBooksData &&
+                pdfBooksData.map((bookData, index) => (
+                  <LibraryItem
+                    key={index}
+                    pdfFilePath={bookData.file_path}
+                    pdfNumPages={bookData.num_pages}
+                    pdfTitle={bookData.title}
+                    pdfThumbnailURL={bookData.thumbnail_path}
+                  />
+                ))}
+            </div>
+          </Stack>
+        </>
       )}
     </>
   )
 }
 
-const LibrayAddItem: React.FC = () => {
+interface LibraryItemProps {
+  pdfTitle: string | null
+  pdfFilePath: string
+  pdfNumPages: number
+  pdfThumbnailURL: string
+}
+
+export const LibraryItem: React.FC<LibraryItemProps> = ({
+  pdfTitle,
+  pdfFilePath,
+  pdfNumPages,
+  pdfThumbnailURL
+}) => {
+
+  console.log(pdfTitle, pdfFilePath, pdfNumPages, pdfThumbnailURL)
+
+  //const handleClick = (data: string): void => {
+  //  console.log(`Navigating to Reader with PDF data`)
+  //  navigate(`/reader`, {state: {data}})
+  //}
+
+  /*
+  const handleOpenPdf = (): void => {
+    // Open the PDF in the browser
+    const pdfUrl = `app://${pdfFilePath}` // You can use the full path here
+    console.log(pdfUrl)
+    window.open(pdfUrl, '_blank') // Open in a new browser tab
+  }
+  */
+
   return (
     <>
-      <div className="library-item-outer">
-        <div className="library-item-image-add">
-          <IoIosAddCircleOutline />
+      <Paper pt="md" pl="md" pr="md" pb="xs" shadow="sm" radius="md" withBorder className={classes.item} >
+        <div className={classes['thumbnail-box']}>
+          <Image
+            fit="contain"
+            className={classes.thumbnail}
+            radius="md"
+            src={pdfThumbnailURL}
+          />
         </div>
-      </div>
+        <Text p={5} className={classes.title}>
+            {typeof pdfTitle !== 'undefined' ? pdfTitle : 'No title found'}
+        </Text>
+      </Paper>
     </>
   )
 }
+
+
