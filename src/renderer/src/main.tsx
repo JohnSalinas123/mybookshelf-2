e6aca1b4a1e151f2739cf0a463cba8e3868f7bc4
JohnSalinas123 <salinasjohn257@gmail.com>
2025-02-10 16:56:30 -0800

    changed BroserRouter to HashRouter for routing to work in desktop setting

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,15 +2,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 
-import '@mantine/core/styles.css';
+import '@mantine/core/styles.css'
+
+//const theme = createTheme({
+//
+//})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <MantineProvider defaultColorScheme="dark">
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </MantineProvider>
 )
