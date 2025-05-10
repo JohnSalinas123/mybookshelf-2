0c6e3090f7e0d0cd6c17fe7174f535aeabb34e93
JohnSalinas123 <salinasjohn257@gmail.com>
2025-05-09 18:34:33 -0700

    moved titlebar to be below OS default title bar for more consistency through different OSs

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,24 +4,34 @@ import { LibraryPage } from './components/LibraryPage'
 
 import classes from './App.module.css'
 import { ReaderPage } from './components/ReaderPage'
-import { TitleBar } from './components/TitleBar'
+import { TitleBar } from './components/TitleBar/TitleBar'
 import { useState } from 'react'
 import { useComputedColorScheme, useMantineColorScheme } from '@mantine/core'
-
 
 function App(): JSX.Element {
   const [titleBarControls, setTitleBarControls] = useState<React.ReactNode>(null)
 
-  const { setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
-  
+  const { setColorScheme } = useMantineColorScheme()
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true })
+
   return (
     <>
-      <TitleBar controls={titleBarControls} setColorScheme={setColorScheme} computedColorScheme={computedColorScheme}/>
+      <TitleBar
+        controls={titleBarControls}
+        setColorScheme={setColorScheme}
+        computedColorScheme={computedColorScheme}
+      />
       <div className={classes['main-container']}>
         <Routes>
-          <Route index path="/" element={<LibraryPage setTitleBarControls={setTitleBarControls}/>} ></Route>
-          <Route path="/reader" element={<ReaderPage setTitleBarControls={setTitleBarControls}/>}></Route>
+          <Route
+            index
+            path="/"
+            element={<LibraryPage setTitleBarControls={setTitleBarControls} />}
+          ></Route>
+          <Route
+            path="/reader"
+            element={<ReaderPage setTitleBarControls={setTitleBarControls} />}
+          ></Route>
         </Routes>
       </div>
     </>
